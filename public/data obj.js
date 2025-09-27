81441ca459559af035f54172efe43d3c7876553c
yadav412 <yadavsingh3012@gmail.com>
2025-09-27 12:48:43 -0700

    feat: data

---
+++ public/data obj.js
@@ -0,0 +1,36 @@
+const merccode = []
+const merc = []
+
+const redbullcode=[ENR,EA,HEINY,LVMUY,V,YETI,T,CDW,XOM,SIE.SG,ZM]
+const redbull=["Armor All -> Energizer Holdings, Inc", "EA Sports -> Electronic Arts Inc.", "Heineken -> Heineken N.V", "TAG Heuer -> LVMH",
+   "Visa -> Visa Inc."," YETI -> YETI Holdings Inc.",'AT&T -> AT&T','CDW -> CDW Corporation','Esso -> Exxon Mobil Corporation',
+  'Siemens -> Siemens AG','Zoom -> Zoom Communications']
+
+
+const williamscode=[SGHC,TEAM, ENR,0,KMTUY,THG.L ]
+const williams=['Betway-> Super Group','Atlassian-> Atlassian Corporatio','Duracell-> Energizer Holdings,Inc.','Life Fitness -> Life Time Group Holdings '
+    , 'Komatsu -> Komatsu Ltd.' , 'My Protein -> THG Plc' ]
+
+
+const rbcodes=[]
+const rb=[]
+
+const saubercodes= []
+const sauber=[]
+
+const ferraricodes=[]
+const ferrari=[]
+
+const alpinecodes=[]
+const alpine=[]
+
+const haascodes =[]
+const haas=[]
+
+const mclaren =[]
+const mclarencode =[]
+
+const astoncodes =[]
+const aston =[]
+
+
