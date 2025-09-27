2b41edd8927053d454e64461c4feea30150736f1
ClarkNoveros-SFUCS <cdn5@sfu.ca>
2025-09-27 15:16:29 -0700

    uiu

--- public/index.js
+++ public/index.js
@@ -36,7 +36,7 @@ fetchData();
 async function fetchData() {
 
   try{
-    const response = await fetch("https://eodhd.com/api/fundamentals/AAPL.US?api_token=demo&fmt=json")
+    const response = await fetch("https://eodhd.com/api/fundamentals/AAPL.US?api_token=demo&fmt=json");
 
     if(!response.ok){
       throw new Error("Could not fecth resource");
@@ -44,6 +44,24 @@ async function fetchData() {
     }
     const data = await response.json();
     console.log(data);
+
+    const code = data.General.Code;
+    const name = data.General.Name;
+    const exchange = data.General.Exchange;
+    const wallStreetTarget = data.Highlights.WallStreetTargetPrice;
+
+    console.log("Code:", code);
+    console.log("Name:", name);
+    console.log("Exchange:", exchange);
+    console.log("Wall Street Target Price:", wallStreetTarget);
+
+    const drop = document.getElementById("drop");
+    drop.innerHTML = `
+      <a href="#">Code: ${code}</a>
+      <a href="#">Name: ${name}</a>
+      <a href="#">Exchange: ${exchange}</a>
+      <a href="#">Target Price: $${wallStreetTarget}</a>
+    `;
 
   }
 
