489ae6e15c9a6592dcec6c97f3de3bc0f634de02
yadav412 <yadavsingh3012@gmail.com>
2025-09-27 16:45:23 -0700

    json add

--- public/index.js
+++ public/index.js
@@ -31,52 +31,7 @@ window.onclick = function (event)
     }
 }
 
-fetchData(symbol);
 
-async function fetchData(symbol) {
-
-  try{
-    const response = await fetch("https://eodhd.com/api/fundamentals/${symbol}?api_token=demo&fmt=json");
-
-
-    if(!response.ok){
-      throw new Error("Could not fetch resource");
-
-    }
-    const data = await response.json();
-
-    console.log(data);
-
-    const code1 = data.General.Code;
-    const name1 = data.General.Name;
-    const exchange1 = data.General.Exchange;
-    const wallStreetTarget1 = data.Highlights.WallStreetTargetPrice;
-
-
-
-    console.log("Code:", code1);
-    console.log("Name:", name1);
-    console.log("Exchange:", exchange1);
-    console.log("Wall Street Target Price:", wallStreetTarget1);
-
-    const drop = document.getElementById('drop');
-    drop.innerHTML = `
-      <a href="#"> Name: ${name1} Code: ${code1}   Exchange: ${exchange1}   Price: $${wallStreetTarget1}</a>
-    
-    `;
-
-  }
-
-  catch(error){
-    console.error(error);
-
-  }
-  
-}
-
-window.onload=()=> {
-    fetchData('HPQ')
-}
 
 
 
@@ -84,28 +39,30 @@ async function loadTeams() {
     try {
         const response = await fetch('sponsors.json');
         if (!response.ok) throw new Error("Could not fetch JSON file");
-        const data = await response.json();
 
+        const data = await response.json();
         const teamNames = Object.keys(data);
+
         teamNames.forEach((teamName, index) => {
             const dropId = `drop${index + 1}`;
             const drop = document.getElementById(dropId);
-            if (drop && data[teamName].length > 0) {
-                // Populate dropdown links
-                drop.innerHTML = data[teamName].map(sponsor => `
-                    <a href="#" class="fontbody">
-                        ${sponsor.Sponsor} | Code: ${sponsor.Code} | Price: $${sponsor.Price}
-                    </a>
-                `).join('');
-            } else if (drop) {
-                drop.innerHTML = `<a href="#" class="fontbody">No sponsors available</a>`;
+
+            if (drop) {
+                if (data[teamName].length > 0) {
+                    drop.innerHTML = data[teamName].map(sponsor => `
+                        <a href="#" class="fontbody">
+                            ${sponsor.Sponsor} | Code: ${sponsor.Code} | Price: $${sponsor.Price}
+                        </a>
+                    `).join('');
+                } else {
+                    drop.innerHTML = `<a href="#" class="fontbody">No sponsors available</a>`;
+                }
             }
         });
-
     } catch (error) {
         console.error(error);
     }
 }
 
-// Call on page load
+// Load JSON on page load
 window.onload = loadTeams;
--- public/index2.js
+++ public/index2.js
@@ -248,3 +248,46 @@ window.onclick = function (event) {
         }
     }
 }
+
+fetchData(symbol);
+
+async function fetchData(symbol) {
+
+  try{
+    const response = await fetch("https://eodhd.com/api/fundamentals/${symbol}?api_token=demo&fmt=json");
+
+
+    if(!response.ok){
+      throw new Error("Could not fetch resource");
+
+    }
+    const data = await response.json();
+
+    console.log(data);
+
+    const code1 = data.General.Code;
+    const name1 = data.General.Name;
+    const exchange1 = data.General.Exchange;
+    const wallStreetTarget1 = data.Highlights.WallStreetTargetPrice;
+
+
+
+    console.log("Code:", code1);
+    console.log("Name:", name1);
+    console.log("Exchange:", exchange1);
+    console.log("Wall Street Target Price:", wallStreetTarget1);
+
+    const drop = document.getElementById('drop');
+    drop.innerHTML = `
+      <a href="#"> Name: ${name1} Code: ${code1}   Exchange: ${exchange1}   Price: $${wallStreetTarget1}</a>
+    
+    `;
+
+  }
+
+  catch(error){
+    console.error(error);
+
+  }
+  
+}
